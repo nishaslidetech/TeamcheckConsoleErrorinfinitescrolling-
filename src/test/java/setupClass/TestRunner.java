469164b72421f7c9621471bff53b39ec86035931
Nisha Dhiman <91591950+nishaslidetech@users.noreply.github.com>
2022-02-21 19:14:06 +0530

    Update TestRunner.java

--- src/test/java/setupClass/TestRunner.java
+++ src/test/java/setupClass/TestRunner.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 
 //@RunWith(Cucumber.class)
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "." }, glue = { "stepDefination" }, tags= {"@BlogPage"},
+@CucumberOptions(features = { "." }, glue = { "stepDefination" }, //tags= {"@BlogPage"},
 		plugin = { "pretty", "html:target/site/cucumber-pretty", "json:target/cucumber/cucumber.json",
 				"usage:target/usage.jsonx", "junit:target/cucumber.xml" })
 
